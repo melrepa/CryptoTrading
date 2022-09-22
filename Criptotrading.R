91ebdd22c474aa30ce64bdc8cfe4f90628910bbd
melrepa <melrepa7@gmail.com>
2022-09-21 19:05:56 -0700

    ipdt

--- Criptotrading.R
+++ Criptotrading.R
@@ -41,13 +41,8 @@ repeat {
   
   # Telegram fields
   
-  token <- "5714075349:AAER6IE28EsAFPAeqroEWcwgTy38RxTu5Pw"
-  
   #Inicializar un chat en Telegram con el bot `R_projectDEM_bot`
-  bot <- Bot(token = token)
-  updates <- bot$get_updates()
-  chat_id <- updates[[1L]]$from_chat_id()
-  
+
   
   # Generate new data every minute
   
